6fd2821ab5fbcf69f61f6d6a7f7fa3465f5dfd4e
Halil Ibrahim Oztekin <35626045+halilibrahimoztekin@users.noreply.github.com>
2021-11-03 16:09:30 +0300

    Initial Commit

---
+++ RestAPIPracticesWithAlamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestAPIPracticesWithAlamofire
+//
+//  Created by Halil İbrahim Öztekin on 3.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
