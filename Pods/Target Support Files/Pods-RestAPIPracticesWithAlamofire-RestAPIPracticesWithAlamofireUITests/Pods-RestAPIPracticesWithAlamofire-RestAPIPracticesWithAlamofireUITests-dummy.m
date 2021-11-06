bec534cd4f40fd40036304e369b3bb49d39f0227
Halil Ibrahim Oztekin <35626045+halilibrahimoztekin@users.noreply.github.com>
2021-11-06 17:23:44 +0300

    core group

---
+++ Pods/Target Support Files/Pods-RestAPIPracticesWithAlamofire-RestAPIPracticesWithAlamofireUITests/Pods-RestAPIPracticesWithAlamofire-RestAPIPracticesWithAlamofireUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RestAPIPracticesWithAlamofire_RestAPIPracticesWithAlamofireUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RestAPIPracticesWithAlamofire_RestAPIPracticesWithAlamofireUITests
+@end
